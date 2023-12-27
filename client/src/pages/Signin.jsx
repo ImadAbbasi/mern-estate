aafdb1161d2f2c88dcdc2f7b4d6276a7d8927f79
ImadAbbasi <imadurrehman033@gmail.com>
2023-12-27 19:15:55 +0500

    Completed profile page UI

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -62,7 +62,7 @@ export default function SignIn() {
         />
         <button
           disabled={loading}
-          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 duration-200 disabled:opacity-50"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 duration-200 disabled:opacity-80"
         >
           {loading ? `Loading...` : `Sign in`}
         </button>

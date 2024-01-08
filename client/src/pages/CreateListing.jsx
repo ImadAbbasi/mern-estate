bcb352c509c20f45a526fc756c3ec0ba8af4ea8d
ImadAbbasi <imadurrehman033@gmail.com>
2024-01-08 09:50:23 +0500

    Created the listing item component and mapped the listings

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -11,6 +11,8 @@ import { useNavigate } from "react-router-dom";
 
 export default function CreateListing() {
   const { currentuser } = useSelector((state) => state.user);
+  console.log(currentuser._id);
+  console.log(currentuser.rest._id);
   const navigate = useNavigate();
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
@@ -139,7 +141,7 @@ export default function CreateListing() {
         },
         body: JSON.stringify({
           ...formData,
-          userRef: currentuser._id,
+          userRef: currentuser._id || currentuser.rest._id,
         }),
       });
       const data = await res.json();
